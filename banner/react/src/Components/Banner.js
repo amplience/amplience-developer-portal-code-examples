1d36d2db44ff6cebc5957d1182583d185337a222
Shelley Weeks <sweeks@amplience.com>
2022-05-31 17:29:33 +0100

    feat(react banner): add banner example

---
+++ banner/react/src/Components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './banner.css';
+
+const Banner = ({ title, subtitle, image, link }) => {
+  const src = image.url().width(1200).height(680).build();
+
+  return (
+    <section className="banner">
+      <header>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </header>
+      <img src={src} alt="" />
+      <a href={link.url}>{link.title}</a>
+    </section>
+  );
+};
+
+export default Banner;
