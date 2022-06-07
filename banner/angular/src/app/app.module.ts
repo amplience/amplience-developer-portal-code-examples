292d9869f3298b69c5f9849f7273835a93cd1c68
Shelley Weeks <sweeks@amplience.com>
2022-06-07 14:18:08 +0100

    fix(angular banner): prevent warning

--- banner/angular/src/app/app.module.ts
+++ banner/angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BannerComponent } from '../Banner/banner.component';
+import { BannerComponent } from '../banner/banner.component';
 
 @NgModule({
   declarations: [AppComponent, BannerComponent],
