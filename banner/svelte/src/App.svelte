fd486293db5fbc5de696cfd1b5537b738217db22
Shelley Weeks <sweeks@amplience.com>
2022-06-08 17:46:45 +0100

    fix(sveltejs banner): improvement based on pr feedback

--- banner/svelte/src/App.svelte
+++ banner/svelte/src/App.svelte
@@ -1,24 +1,14 @@
 <script>
   import Banner from './Banner.svelte';
-  import { onMount } from 'svelte';
   import { ContentClient } from 'dc-delivery-sdk-js';
-
-  let banner = null;
 
   const client = new ContentClient({
     hubName: 'ampengineering',
   });
-
-  const deliveryKey = 'banner-example';
-
-  onMount(async () => {
-    const { body } = await client.getContentItemByKey(deliveryKey);
-    banner = body;
-  });
 </script>
 
 <main>
-  {#if banner}
+  {#await client.getContentItemByKey('banner-example') then { body: banner }}
     <Banner {...banner} />
-  {/if}
+  {/await}
 </main>
