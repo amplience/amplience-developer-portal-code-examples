1dd28946311564078f90142ccd0de399a581ae6b
Joe Warner <jwarner@amplience.com>
2022-07-21 16:09:09 +0100

    fix: promisify generating examples fixed some typos

--- generate-sandpack-config.mjs
+++ generate-sandpack-config.mjs
@@ -1,92 +1,95 @@
 import _ from 'lodash';
 
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs/promises';
+
+import { existsSync } from 'fs';
 
 const examples = ['banner'];
 
 console.log('Sandpack config factory 🏭');
 
-examples.forEach(example => {
+await Promise.all(examples.map(createExample));
+
+async function createExample(example) {
   let __dirname = path.join(example);
 
-  const framworks = fs.readdirSync(__dirname);
+  const frameworks = await fs.readdir(__dirname);
 
-  framworks.map(framwork => path.join(__dirname, framwork)).map(createConfig);
-});
+  await Promise.all(
+    frameworks.map(async framwork => {
+      const dir = path.join(__dirname, framwork);
 
-function createConfig(dirname) {
-  let __dirname = path.join(dirname);
+      return createConfig(dir);
+    })
+  );
+}
+
+async function createConfig(basePath) {
+  let __dirname = path.join(basePath);
 
   const mapFileNameToCode = mapFileNameToCodeFactory(__dirname);
-  const readAllFilesFromBlob = readAllFilesFromBlobFactory(mapFileNameToCode);
+  const readAllFilesFromBlob = readAllFilesFromBlobFactory();
 
-  try {
-    const config = fs.readFileSync(
-      path.join(__dirname, 'sandpack.config.json'),
-      'utf8'
-    );
+  const [config, packageJson] = await Promise.all([
+    fs.readFile(path.join(__dirname, 'sandpack.config.json'), 'utf8'),
+    fs.readFile(path.join(__dirname, 'package.json'), 'utf8'),
+  ]);
 
-    const packageJson = fs.readFileSync(
-      path.join(__dirname, 'package.json'),
-      'utf8'
-    );
+  const parsedPackage = JSON.parse(packageJson);
+  const parsedConfig = JSON.parse(config);
 
-    const parsedPackage = JSON.parse(packageJson);
-    const parsedConfig = JSON.parse(config);
+  const { files } = parsedConfig;
 
-    const { files } = parsedConfig;
+  let expandedFiles = [];
 
-    let expandedFiles = [];
-
-    if (_.isArray(files)) {
-      expandedFiles = files.reduce(mapFileNameToCode, {});
-    } else {
-      expandedFiles = readAllFilesFromBlob(path.join(__dirname, files));
-    }
-
-    const dirname = path.join(__dirname, '.sandpack');
-
-    if (!fs.existsSync(dirname)) {
-      fs.mkdirSync(dirname);
-    }
-
-    const generated = {
-      ...parsedConfig,
-      files: expandedFiles,
-      customSetup: {
-        ...parsedConfig.customSetup,
-        ...parsedPackage,
-      },
-    };
-
-    const generatedPath = path.join(
-      __dirname,
-      '.sandpack',
-      'sandpack.config.json'
-    );
-
-    fs.writeFileSync(generatedPath, JSON.stringify(generated));
-
-    console.log(`created: ${generatedPath}`);
-  } catch (err) {
-    console.error(err.message);
+  if (_.isArray(files)) {
+    expandedFiles = await files.reduce(mapFileNameToCode, Promise.resolve({}));
+  } else {
+    expandedFiles = await readAllFilesFromBlob(path.join(__dirname, files));
   }
+
+  const dirname = path.join(__dirname, '.sandpack');
+
+  if (!existsSync(dirname)) {
+    await fs.mkdir(dirname);
+  }
+
+  const generated = {
+    ...parsedConfig,
+    files: expandedFiles,
+    customSetup: {
+      ...parsedConfig.customSetup,
+      ...parsedPackage,
+    },
+  };
+
+  const generatedPath = path.join(
+    __dirname,
+    '.sandpack',
+    'sandpack.config.json'
+  );
+
+  await fs.writeFile(generatedPath, JSON.stringify(generated));
+
+  console.log(`created: ${generatedPath}`);
 }
 
 function mapFileNameToCodeFactory(__dirname) {
-  return function mapFileNameToCode(fileMap, file) {
+  return async function mapFileNameToCode(fileMapPromise, file) {
+    const fileMap = await fileMapPromise;
+
     if (_.isPlainObject(file)) {
       const { title } = file;
 
-      const code = fs.readFileSync(path.join(__dirname, title), 'utf8');
+      const code = await fs.readFile(path.join(__dirname, title), 'utf8');
 
       return {
         ...fileMap,
         [title]: code,
       };
     }
-    const code = fs.readFileSync(path.join(__dirname, file), 'utf8');
+    const code = await fs.readFile(path.join(__dirname, file), 'utf8');
 
     return {
       ...fileMap,
@@ -95,24 +98,29 @@ function mapFileNameToCodeFactory(__dirname) {
   };
 }
 
-function readAllFilesFromBlobFactory(__dirname = '.') {
+function readAllFilesFromBlobFactory() {
   const mapFileNameToCode = mapFileNameToCodeFactory('.');
 
-  return function readAllFilesFromBlob(dirname = './src', files = []) {
+  return async function readAllFilesFromBlob(dirname = './src', files = []) {
     const directory = path.join(dirname);
 
-    const fileNames = fs.readdirSync(directory);
+    const fileNames = await fs.readdir(directory);
 
-    fileNames.forEach(file => {
-      const absolute = path.join(directory, file);
+    await Promise.all(
+      fileNames.map(async file => {
+        const absolute = path.join(directory, file);
+        const stats = await fs.stat(absolute);
 
-      if (fs.statSync(absolute).isDirectory()) {
-        readAllFilesFromBlob(absolute, files);
-      } else {
-        files.push(absolute);
-      }
-    });
+        if (stats.isDirectory()) {
+          await readAllFilesFromBlob(absolute, files);
+        } else {
+          files.push(absolute);
+        }
+      })
+    );
 
-    return files.map(fileName => `/${fileName}`).reduce(mapFileNameToCode, {});
+    const remapedPaths = files.map(fileName => `/${fileName}`);
+
+    return remapedPaths.reduce(mapFileNameToCode, Promise.resolve({}));
   };
 }
